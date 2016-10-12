abcb66d3debca0f2145dc1c268b4ca35b7b61c21
Pawel Szymanski <p.szymanski@nomtek.com>
2016-10-12 10:59:34 +0200

    Minor update to mappers

--- api/src/main/java/com/creatubbles/api/response/SameResponseMapper.java
+++ api/src/main/java/com/creatubbles/api/response/SameResponseMapper.java
@@ -4,6 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import retrofit2.Response;
 
+/**
+ * Class mapping response to the same type as one defined in Retrofit service.
+ * Basically this just unwraps body from Response class.
+ */
 public class SameResponseMapper<T> extends BaseResponseMapper<T, T> {
     public SameResponseMapper(ObjectMapper objectMapper, ResponseCallback<T> responseCallback) {
         super(objectMapper, responseCallback);
