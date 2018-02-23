b829fdd84dd4bf7e69b1a3345756186b81f2cdde
Pawel Szymanski <p.szymanski@nomtek.com>
2018-02-23 10:35:31 +0100

    Add GIF ContentType

--- api/src/main/java/com/creatubbles/api/ContentType.java
+++ api/src/main/java/com/creatubbles/api/ContentType.java
@@ -5,11 +5,26 @@ package com.creatubbles.api;
  */
 public enum ContentType {
 
-    URL_ENCODED("application/x-www-form-urlencoded;charset=UTF-8"), VND_JSON("application/vnd" +
-            ".api+json"), JSON("application/json"), PNG("png"), JPG("jpg"), JPEG
-            ("jpeg"), H264("h264"), MPEG4("mpeg4"), WMV("wmv"), WEBM
-            ("webm"), FLV("flv"), OGG("ogg"), OGV("ogv"), MP4
-            ("mp4"), M4V("m4v"), F4V("f4v"), MOV("mov"), ZIP("zip");
+    URL_ENCODED("application/x-www-form-urlencoded;charset=UTF-8"),
+    VND_JSON("application/vnd.api+json"),
+    JSON("application/json"),
+    PNG("png"),
+    JPG("jpg"),
+    JPEG("jpeg"),
+    H264("h264"),
+    MPEG4("mpeg4"),
+    WMV("wmv"),
+    WEBM("webm"),
+    FLV("flv"),
+    OGG("ogg"),
+    OGV("ogv"),
+    MP4("mp4"),
+    M4V("m4v"),
+    F4V("f4v"),
+    MOV("mov"),
+    ZIP("zip"),
+    GIF("gif");
+
     String res;
 
     ContentType(String res) {
